d4ca11d792b6760a8854b2d918a40014e81d6d7b
Xerrors <xerrors@163.com>
2019-01-23 18:50:03 +0800

    update in VScode

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -134,27 +134,23 @@ export default {
     },
     // 执行用户登录操作
     handleLogin() {
-      // this.$refs.loginForm.validate(valid => {
-      //   if (valid) {
-      //     this.loading = true;
-      //     this.$store
-      //       .dispatch("Login", this.loginForm)
-      //       .then(() => {
-      //         this.loading = false;
-      //         this.$router.push({ path: this.redirect || "/" });
-      //       })
-      //       .catch(() => {
-      //         this.loading = false;
-      //       });
-      //   } else {
-      //     console.log("error submit!!");
-      //     return false;
-      //   }
-      // });
-      this.loding = true;
-      axios.get('https://vintag-a5287.firebaseio.com/users.json')
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.$store
+            .dispatch("Login", this.loginForm)
+            .then(() => {
+              this.loading = false;
+              this.$router.push({ path: this.redirect || "/" });
+            })
+            .catch(() => {
+              this.loading = false;
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
     forgetPasswd() {
       this.$alert("这个我也没有办法啊", "忘记密码", {
