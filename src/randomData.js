cc24afecf781c8b28c74560564c4317d0670a820
Xerrors <xerrors@163.com>
2019-01-26 17:52:33 +0800

    add random data file

---
+++ src/randomData.js
@@ -0,0 +1,32 @@
+export function randomData() {
+  var randData = {}
+  // 标题
+  randData.title = '随机图表' + Math.floor((Math.random() * 90 + 10))
+  // 图例
+  randData.legend = []
+  var legendLen = Math.floor((Math.random() * 3 + 2))
+  for (var x = 1; x <= legendLen; x++) {
+    randData.legend.push('数据' + x)
+  }
+  // 数据
+  randData.data = []
+  var dataLength = Math.floor((Math.random() * 10 + 8))
+  for (var i = 0; i < randData.legend.length; i++) {
+    var data = []
+    for (var j = 0; j < dataLength; j++) {
+      data.unshift(Math.floor((Math.random() * 100 + 100)))
+    }
+    randData.data.push(data)
+  }
+  // 坐标轴数据
+  randData.axis = {
+    x: [],
+    y: []
+  }
+  randData.axis.x = []
+  for (var m = 1; m <= dataLength; m++) {
+    randData.axis.x.push(m)
+  }
+  console.log(randData)
+  return randData
+}
--- src/views/charts/demoChart.vue
+++ src/views/charts/demoChart.vue
@@ -7,6 +7,7 @@
 <script>
 /* eslint-disable */
 import echarts from "echarts";
+import { randomData } from '@/randomData.js'
 export default {
   name: "demoChart",
   components: {},
@@ -14,27 +15,22 @@ export default {
     return {
       chart: null,
       chartType: "line",
-      data: [],
-      axisData: {
-        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-      },
-      legend: ["A", "B", "C"]
     };
   },
   computed: {
-    itemNum() {
-      return this.legend.length;
+    recv() {
+      return randomData()
     },
     theme() {
       return this.$store.getters.theme;
     },
     mySeries() {
       var mySeries = [];
-      for (var i = 0; i < this.itemNum; i++) {
+      for (var i = 0; i < this.recv.legend.length; i++) {
         mySeries.push({
           type: this.chartType,
-          name: this.legend[i],
-          data: this.data[i]
+          name: this.recv.legend[i],
+          data: this.recv.data[i]
         });
       }
       return mySeries;
@@ -42,22 +38,22 @@ export default {
     options() {
       return {
         title: {
-          text: "十二个季度的销售额",
+          text: this.recv.title,
           top: "10px",
           left: "20px"
         },
         legend: {
-          x: "center",
-          data: this.legend,
-          top: "10px"
+          x: "right",
+          data: this.recv.legend,
+          top: "10px",
+          right: "10px"
         },
         tooltip: {
           trigger: "axis"
         },
         xAxis: {
           type: "category",
-          boundaryGap: false,
-          data: this.axisData.x
+          data: this.recv.axis.x
         },
         yAxis: {},
         series: this.mySeries
@@ -70,20 +66,10 @@ export default {
       this.chart.showLoading();
       this.chart.setOption(this.options);
       this.chart.hideLoading();
-    },
-    setRandomData() {
-      for(var i = 0; i < this.itemNum; i++){
-        var data = [];
-        for(var j = 0; j < 12; j ++){
-          data.push(Math.random()*100 + 200)
-        }
-        this.data.push(data)
-      }
     }
   },
   mounted() {
     this.$nextTick(function() {
-      this.setRandomData();
       this.drawGraph("demoChart");
     });
   }
