2052cf5c77c521edbd3ca51eacdfa5724c60fd6c
Xerrors <xerrors@163.com>
2019-01-29 02:13:52 +0800

    change the slideBar style

--- src/views/layout/components/Sidebar/Avatar.vue
+++ src/views/layout/components/Sidebar/Avatar.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="avatar">
-    <div class="avatar__nav">
-      <span class="avate__nav__svg">
+    <div class="avatar__nav" @click="logout">
+      <span class="avatar__nav-svg">
         <svg-icon icon-class="log-out"/>
       </span>
-      <span class="avatar__nav__text">
-        <p>LOGOUT</p>
-      </span>
+      <div class="avatar__nav-text">LOGOUT</div>
     </div>
     <div class="avatar__img">
       <img src="@/assets/img/avatar.png">
@@ -26,6 +24,30 @@ export default {
       name: "瓦塔撒",
       identity: "user"
     };
+  },
+  methods: {
+    logout() {
+      this.$confirm("确定要登录当前帐号吗?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$store.dispatch("LogOut").then(() => {
+            location.reload(); // 为了重新实例化vue-router对象 避免bug
+          });
+          this.$message({
+            type: "success",
+            message: "登出成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消登出！"
+          });
+        });
+    }
   }
 };
 </script>
@@ -38,25 +60,36 @@ export default {
   border-bottom-color: rgb(28, 28, 28);
   border-bottom-width: 2px;
   &__nav {
-    width: 100%;
+    display: flex;
+    align-content: center;
+    width: 120px;
     height: 50px;
-    &__svg {
-      font-size: 30px;
-      color: rgb(119, 119, 119);
-    }
-    &__text p {
-      font-family: "SourceHanSansSC";
-      color: rgb(138, 138, 138);
-      line-height: 1.2;
-    }
+    cursor: pointer;
   }
+  &__nav-svg {
+    margin-top: 15px;
+    margin-left: 15px;
+    font-size: 20px;
+    color: rgb(119, 119, 119);
+  }
+  &__nav-text {
+    margin-top: 20px;
+    text-align: center;
+    font-size: 13px;
+    font-family: "SourceHanSansSC";
+    color: rgb(138, 138, 138);
+    height: 13px;
+  }
+
   &__img {
     width: 100%;
     height: 150px;
     img {
-      width: 100px;
-      height: 100px;
-      margin: auto auto;
+      position: relative;
+      top: 18px;
+      left: 103px;
+      width: 114px;
+      height: 114px;
     }
   }
   &__foo {
@@ -81,4 +114,3 @@ export default {
   }
 }
 </style>
-
